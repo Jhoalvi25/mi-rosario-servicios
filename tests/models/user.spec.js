c16e9e5683dd696112849ab15b2d20d0b3b4c8d0
Jhoalvi25 <jhoalvipereira@gmail.com>
2024-02-02 11:33:04 -0500

    modificaciones

--- tests/models/user.spec.js
+++ tests/models/user.spec.js
@@ -1,7 +1,7 @@
 const { Recipe, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('Recipe model', () => {
+describe('User model', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
@@ -15,7 +15,7 @@ describe('Recipe model', () => {
           .catch(() => done());
       });
       it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Milanesa a la napolitana' });
+        Recipe.create({ name: 'Usuario registrado en la bse de datos' });
       });
     });
   });
--- tests/routes/user.spec.js
+++ tests/routes/user.spec.js
@@ -2,23 +2,23 @@
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
-const { Recipe, conn } = require('../../src/db.js');
+const { User, conn } = require('../../src/db.js');
 
 const agent = session(app);
-const recipe = {
-  name: 'Milanea a la napolitana',
+const user = {
+  name: 'New people register',
 };
 
-describe('Recipe routes', () => {
+describe('User routes', () => {
   before(() => conn.authenticate()
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Recipe.sync({ force: true })
-    .then(() => Recipe.create(recipe)));
-  describe('GET /recipes', () => {
+  beforeEach(() => User.sync({ force: true })
+    .then(() => User.create(user)));
+  describe('Post /register', () => {
     it('should get 200', () =>
-      agent.get('/recipes').expect(200)
+      agent.get('/register').expect(200)
     );
   });
 });
