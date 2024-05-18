0f36332f591c3609a0e9a71142a40bcb68b7f121
Jhoalvi pereira <101520054+Jhoalvi25@users.noreply.github.com>
2024-05-18 15:43:25 -0500

    Update app.js
    all orl acess

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mi-rosario-servicios.onrender.com'); // Aqui puedo cambiar la URL de localhost por la de la app desplegada
+  res.header('Access-Control-Allow-Origin', '*'); // Aqui puedo cambiar la URL de localhost por la de la app desplegada
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
